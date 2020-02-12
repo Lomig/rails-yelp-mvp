4bac9d8d14808e73c41eb8cdb84052d19afcda95
Lomig <dunwael@gmail.com>
2020-02-12 12:13:47 +0100

    New seed of test restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+restaurants = [
+  {
+    name: "La Brigade",
+    address: "1, rue Le Wagon",
+    phone_number: "01 01 01 01",
+    category: "french"
+  },
+  {
+    name: "Léon",
+    address: "2, rue Le Wagon",
+    phone_number: "02 02 02 02",
+    category: "belgian"
+  },
+  {
+    name: "Le Canard Laqué",
+    address: "3, rue Le Wagon",
+    phone_number: "03 03 03 03",
+    category: "chinese"
+  },
+  {
+    name: "Le Weeb",
+    address: "4, rue Le Wagon",
+    phone_number: "04 04 04 04",
+    category: "japanese"
+  },
+  {
+    name: "Mamma Mia",
+    address: "5, rue Le Wagon",
+    phone_number: "05 05 05 05",
+    category: "italian"
+  }
+]
+
+restaurants.each { |r| Restaurant.create(r) }

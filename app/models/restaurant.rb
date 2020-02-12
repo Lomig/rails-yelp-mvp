08672a3e573267f48f2de851eb7344fd91c88b80
Lomig <dunwael@gmail.com>
2020-02-12 11:48:21 +0100

    Model validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
+##
+# Restaurant Model
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+
+  validates :category,
+            inclusion: { in: %w[chinese italian japanese french belgian] }
 end
